eb8d98f72f81eda85a7f3c7128a8b74cf66f6b27
ActuallyJustAVirus <91348480+ActuallyJustAVirus@users.noreply.github.com>
2022-08-25 19:55:54 +0200

    life

--- games/game_of_life/lifescript.js
+++ games/game_of_life/lifescript.js
@@ -1,36 +1,33 @@
 const canvas = document.getElementById("canvas");
-var grf = canvas.getContext("2d");
-var life = [13,23];
 const size = 20;
 
-console.log("test");
-grf.fillStyle = "#FFFFFF";
-grf.fillRect(20, 20,size,size);
-draw();
-function draw() {
-    life.forEach(box => {
-        var x = (box%10)*20;
-        var y = Math.floor(box/10)*20;
-        grf.fillStyle = "#FFFFFF";
-        grf.fillRect(x, y,size,size);
-    });
-}
+var game = new gameoflife();
+game.newrandom(70,30);
+// for (let i = 0; i < 25; i++) {
+//     game.nextgen();
+// }
+
+// game.board = [[0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0], [0,0,1,1,1,1,0,0,0,0], [0,1,0,0,0,1,0,0,0,0], [0,0,0,0,0,1,0,0,0,0], [0,0,0,0,1,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0]];
+setInterval(() => {
+    game.draw();
+    game.nextgen();
+}, 200);
+
 function gameoflife() {
     this.newrandom = function (width,height) {
         this.board = new Array(height);
-        for (let y = 0; y < width; y++) {
+        for (let y = 0; y < height; y++) {
             this.board[y] = new Array(width);
-            for (let x = 0; x < height; x++) {
+            for (let x = 0; x < width; x++) {
                 this.board[y][x] = Math.round(Math.random());
             }
-            
         }
     }
     this.newclean = function (width,height) {
         this.board = new Array(height);
-        for (let y = 0; y < width; y++) {
+        for (let y = 0; y < height; y++) {
             this.board[y] = new Array(width);
-            for (let x = 0; x < height; x++) {
+            for (let x = 0; x < width; x++) {
                 this.board[y][x] = 0;
             }
             
@@ -44,9 +41,51 @@ function gameoflife() {
         for (let y = 0; y < this.board.length; y++) {
             for (let x = 0; x < this.board[y].length; x++) {
                 var neighbors = 0;
-                
+                for (let yy = -1; yy <= 1; yy++) {
+                    for (let xx = -1; xx <= 1; xx++) {
+                        if (yy == 0 && xx == 0) {continue;} 
+                        var ny = y+yy;
+                        var nx = x+xx;
+                        if (ny == -1) {ny = this.board.length-1}
+                        else if (ny == this.board.length) {ny = 0}
+                        if (nx == -1) {nx = this.board[y].length-1}
+                        else if (nx == this.board[y].length) {nx = 0}
+                        if (this.board[ny][nx] == 1) {
+                            neighbors++;
+                        }
+                    }    
+                }
+                var isalive = this.board[y][x];
+                switch (neighbors) {
+                    case 0:
+                    case 1:
+                        isalive = 0;
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        isalive = 1;
+                        break;
+                    default:
+                        isalive = 0;
+                        break;
+                }
+                this.nextboard[y][x] = isalive;
+            } 
+        }
+        this.board = this.nextboard.slice();
+    }
+    this.draw = function () {
+        const context = canvas.getContext("2d");
+        context.clearRect(0,0,canvas.width,canvas.height);
+        context.fillStyle = "#FFFFFF";
+        // console.log(this.board);
+        for (let y = 0; y < this.board.length; y++) {
+            for (let x = 0; x < this.board[y].length; x++) {
+                if (this.board[y][x]) {
+                    context.fillRect(x*size, y*size,size,size);
+                }
             }
-            
         }
     }
 }
