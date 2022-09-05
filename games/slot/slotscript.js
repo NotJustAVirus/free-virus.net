3d42767bedbc2d950021fb961d3cb370bec5bbc2
ActuallyJustAVirus <91348480+ActuallyJustAVirus@users.noreply.github.com>
2022-09-05 15:00:29 +0200

    slot spin

--- games/slot/slotscript.js
+++ games/slot/slotscript.js
@@ -3,18 +3,51 @@ function spin() {
     const wheel2 = document.getElementById("wheel-2");
     const wheel3 = document.getElementById("wheel-3");
     let id = null;
-    let pos = 0;
-    if (wheel1.style.top == "340px") {
-        wheel1.style.top = "0px";
+    let id2 = null;
+    let pos;
+    if (wheel1.style.top) {
+        pos = -parseInt(wheel1.style.top,10);
+    } else {
+        pos = 0;
     }
+    // console.log(wheel1.style.top);
+    // if (wheel1.style.top >= "400px") {
+    //     wheel1.style.top = "0px";
+    // }
     clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 340) {
-        clearInterval(id);
-        } else {
-        pos++; 
-        wheel1.style.top = pos + "px";
+    clearInterval(id2);
+    let speed = 0;
+    let acc = 0.05;
+    let ticks = 100;
+    id = setInterval(accelerate, 5);
+    // acc = -0.01;
+    // id = setInterval(decelerate, 5);
+    function accelerate() {
+        if (ticks <= 0) {
+            acc = -0.01;
+            id2 = setInterval(decelerate, 5);
+            clearInterval(id);
         }
+        if (pos >= 400) {
+        pos = 0;
+        } else {
+        pos += speed; 
+        speed += acc;
+        ticks--;
+        }
+        wheel1.style.top = -pos + "px";
+    }
+    function decelerate() {
+        if (speed <= 0) {
+            acc = 0;
+            clearInterval(id2);
+        }
+        if (pos >= 400) {
+        pos = 0;
+        } else {
+        pos += speed; 
+        speed += acc;
+        }
+        wheel1.style.top = -pos + "px";
     }
 }
