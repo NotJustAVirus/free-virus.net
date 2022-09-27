91cd62c66933d35d651da8d5dec7ee316046599a
ActuallyJustAVirus <91348480+ActuallyJustAVirus@users.noreply.github.com>
2022-09-27 15:31:23 +0200

    cards for blackjack

---
+++ .sql/addthings.sql
@@ -0,0 +1,18 @@
+-- @block
+INSERT INTO games (title, path, description)
+VALUES 
+    
+    -- ("Game of Life","game_of_life","");
+
+-- @block
+INSERT INTO tags (name, color)
+VALUES
+    ("Casino","006300");
+    -- ("Javascript","eee170");
+
+-- @block
+INSERT INTO game_tags (game_id, tag_id)
+VALUES
+    (7,1);
+    -- (1,2),
+    -- (2,2);
