2b532d1dc4354949ad2bae07b959207601936a02
ActuallyJustAVirus <91348480+ActuallyJustAVirus@users.noreply.github.com>
2022-11-11 16:10:53 +0100

    hiding works

--- source/games/sortcards.js
+++ source/games/sortcards.js
@@ -12,10 +12,13 @@ function loadsite() {
         
         for (const card_data of data) {
             var tags = "";
+            var tags_data = [];
             for (const tag of card_data.tags) {
-                tags += `<div style="color: #${tag.color};" class="tag" data-tag="${tag.name.toLowerCase()}">${tag.name}</div>`;
+                const name_lower = tag.name.toLowerCase();
+                tags += `<div style="color: #${tag.color};" class="tag" data-tag="${name_lower}">${tag.name}</div>`;
+                tags_data.push(name_lower);
             }
-            var card = `<div class='column'>
+            var card = `<div class='column' data-tags="${tags_data.toString()}">
                 <div class='card'>
                     <div class='top'>
                         <div class='text'>
@@ -56,7 +59,7 @@ function loadsite() {
         });
 
         selected_tags.append(alltags);
-        selected_tags.children(".tag").hide();
+        selected_tags.children(".tag").addClass("hidden");
         selected_tags.children(".tag").click(function() {
             removesearchtag($(this).attr("data-tag"));
         });
@@ -65,14 +68,41 @@ function loadsite() {
 
 function addsearchtag(tag) {
     const selected_tags = $("#selected_tags");
-    selected_tags.children(`[data-tag='${tag}']`).show();
+    selected_tags.children(`[data-tag='${tag}']`).removeClass("hidden");
+    updatecards();
 }
 
 function removesearchtag(tag) {
     const selected_tags = $("#selected_tags");
-    selected_tags.children(`[data-tag='${tag}']`).hide();
+    selected_tags.children(`[data-tag='${tag}']`).addClass("hidden");
+    updatecards();
 }
 
 function updatecards() {
-    
+    console.log(1);
+    var selected = [];
+    $("#selected_tags").children(".tag").each(function (index, value) {
+        if (!$(this).hasClass("hidden")) {
+            selected.push($(this).attr("data-tag"));
+        }
+    });
+    if (selected.length == 0) {
+        $(".column").each(function (index, value) {
+            $(this).removeClass("hidden");
+            return;
+        });
+    } else {
+        $(".column").each(function (index, value) {
+            this_tags = $(this).attr("data-tags").split(",");
+            // console.log(this_tags);
+            for (const this_tag of this_tags) {
+                if (selected.includes(this_tag)) {
+                    $(this).removeClass("hidden");
+                    return;
+                }
+            }
+            $(this).addClass("hidden");
+            return;
+        });
+    }
 }
