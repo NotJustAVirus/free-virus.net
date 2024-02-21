bc0c29b1bc403a5fd8f71ed844df1707b9c36ce4
JustAVirus <bern64390@gmail.com>
2024-02-21 22:06:42 +0100

    add spin logic

--- source/games/luckywheel/script.js
+++ source/games/luckywheel/script.js
@@ -2,28 +2,43 @@ var spinning = false;
 var money;
 getmoney();
 
+const wheelResults = [
+    "blue",
+    "yellow",
+    "green",
+    "red",
+    "blue",
+    "yellow",
+    "green",
+    "black",
+    "blue",
+    "yellow",
+    "green",
+    "red",
+];
 
-// function getmoney() {
-//     $.ajax(
-//         'casino.php?type=money',
-//         {
-//             success: function(data) {
-//                 money = data;
-//                 updatemoney();
-//             },
-//             error: function() {
-//                 alert('tell the developer his code sucks');
-//             }
-//         }
-//     );
-// }
+
+function getmoney() {
+    $.ajax(
+        '/games/casino.php?type=money',
+        {
+            success: function(data) {
+                money = data;
+                updatemoney();
+            },
+            error: function() {
+                alert('tell the developer his code sucks');
+            }
+        }
+    );
+}
 
 function spin() {
     if (spinning) {
         return;
     }
     spinning = true;
-    spinto(2);
+    spinto(wheelResults[Math.floor(Math.random()*wheelResults.length)]);
     return;
     $.ajax(
         'casino.php?type=play_luckywheel',
@@ -46,30 +61,37 @@ function spinto(result) {
     
     const wheel = document.getElementById("wheelImg");
 
-    if (result == 2) {
-        landon1 = 3;
-    } else if (result == 1) {
-        var land = Math.floor(Math.random()*3);
-        landon1 = land;
-    } else {
-        landon1 = Math.floor(Math.random()*3);
+    var options = [];
+    for (var i = 0; i < wheelResults.length; i++) {
+        if (wheelResults[i] == result) {
+            options.push(i);
+        }
     }
+    var landon = options[Math.floor(Math.random()*options.length)];
 
-    spinwheel(wheel,landon1);
-    setTimeout(() => {
-        if (result == 2) {
+    var doneSpinng = new Promise((resolve, reject) => {
+        spinwheel(wheel,landon, resolve);
+    });
+    doneSpinng.then(() => {
+        console.log(result);
+        if (result == "red") {
             // console.log("you win!");
             money += 1000;
             updatemoney();
-        } else if (result == 1) {
+        } else if (result == "green") {
             money += 250;
+            updatemoney();
+        } else if (result == "yellow") {
+            money += 150;
+            updatemoney();
+        } else if (result == "blue") {
+            money += 50;
             updatemoney();
         }
         spinning = false;
-    }, 6000);
-    
+    });
 
-    function spinwheel(wheel,landon) {
+    function spinwheel(wheel, landon, resolve) {
         let id = null;
         let id2 = null;
         let pos;
@@ -82,20 +104,19 @@ function spinto(result) {
         clearInterval(id2);
         let speed = 0;
         let acc = 0.09;
-        let ticks = 60;
+        let ticks = 100;
         id = setInterval(accelerate, 5);
         function accelerate() {
             if (ticks <= 0) {
-                // pos = 75+(100*landon);
+                pos = -22 + (landon*30) + 5 + Math.floor(Math.random()*20);
                 id2 = setInterval(decelerate, 5);
                 clearInterval(id);
             }
-            if (pos >= 360) {
-            pos = pos-360;
-            } else {
             pos += speed; 
             speed += acc;
             ticks--;
+            if (pos >= 360) {
+                pos = pos-360;
             }
             wheel.style.rotate = -pos + "deg";
             console.log("change");
@@ -104,15 +125,18 @@ function spinto(result) {
             if (speed <= 0.01) {
                 acc = 0;
                 clearInterval(id2);
+                resolve();
             }
-            if (pos >= 360) {
-            pos = pos-360;
-            } else {
             pos += speed; 
-            speed = 0.995;
+            if (pos >= 360) {
+                pos = pos-360;
             }
+            var decelerate = speed * 0.005;
+            if (decelerate > 0.01) {
+                decelerate = 0.01;
+            }
+            speed -= decelerate;
             wheel.style.rotate = -pos + "deg";
-            console.log("change");
         }
 
     }
