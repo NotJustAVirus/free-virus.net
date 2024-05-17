07a54085a98cc73c22e8399abc32a7b6c7b50922
JustAVirus <bern64390@gmail.com>
2024-05-17 22:24:24 +0200

    Remove unnecessary SQL files and tables

--- .sql/casino.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `casino_user` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `money` INT NOT NULL DEFAULT 500
-);
--- .sql/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(255) UNIQUE NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `authLevel` INT NOT NULL DEFAULT 0
-);
-
