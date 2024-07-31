8602863832482a63ef7eabf16f5fdd0644d383bc
JustAVirus <bern64390@gmail.com>
2024-07-31 23:03:34 +0200

    enable being able to drag to draw

--- source/games/skin_editor/MouseListener.js
+++ source/games/skin_editor/MouseListener.js
@@ -9,57 +9,69 @@ export class MouseListener {
         this.controls = controls;
 		this.raycaster = new THREE.Raycaster();
 		this.mouse = new THREE.Vector2();
-		this.window.addEventListener('click', this.click.bind(this));
+		// this.window.addEventListener('click', this.click.bind(this));
 		this.window.addEventListener('mousedown', this.mouseDown.bind(this));
+        this.window.addEventListener('mousemove', this.mouseMove.bind(this));
         this.window.addEventListener('mouseup', this.mouseUp.bind(this));
         this.callback = null;
+        this.drawing = false;
 	}
 
 	mouseDown(event) {
-		event.preventDefault();
-
-		this.mouse.x = (event.offsetX / this.window.scrollWidth) * 2 - 1;
-		this.mouse.y = - (event.offsetY / this.window.scrollHeight) * 2 + 1;
-
+        event.preventDefault();
+        
+		this.mouse = this.getMouseCoords(event);
+        
         var intersects = this.raycast();
-
+        
         if (intersects.length > 0) {
-            console.log(intersects);
             this.controls.enabled = false;
-        }
-	}
-
-    mouseUp(event) {
-        this.controls.enabled = true
-    }
-
-	click(event) {
-		event.preventDefault();
-
-		if (!((event.offsetX / this.window.scrollWidth) * 2 - 1 == this.mouse.x && - (event.offsetY / this.window.scrollHeight) * 2 + 1 == this.mouse.y)) {
-			return;
-		}
-
-		var intersects = this.raycast();
-
-		if (intersects.length > 0) {
-            var i = 0;
-            for (; i < intersects.length; i++) {
-                if (intersects[i].object.parent.visible == false) {
-                    continue;
-                }
-                break;
-            }
-            var point = intersects[i].uv;
-            point.x = Math.floor(point.x * 64);
-            point.y = 63 - Math.floor(point.y * 64);
+            this.drawing = true;        
+            var point = this.getIntersectPoint(intersects);
+            this.lastPoint = point;
             if (this.callback) {
                 this.callback(point, event.ctrlKey);
             }
-		} else {
-            this.callback(null, event.ctrlKey);
         }
 	}
+
+    mouseMove(event) {
+        if (!this.drawing) {
+            return;
+        }
+        this.mouse = this.getMouseCoords(event);
+        var intersects = this.raycast();
+
+        if (intersects.length > 0) {
+            var point = this.getIntersectPoint(intersects);
+            if (this.lastPoint && point.x == this.lastPoint.x && point.y == this.lastPoint.y) {
+                return;
+            }
+            if (this.callback) {
+                this.callback(point, event.ctrlKey);
+            }
+        }
+    }
+
+    mouseUp(event) {
+        this.drawing = false;
+        var mouse = this.getMouseCoords(event);
+        var intersects = this.raycast();
+
+        if (intersects.length == 0) {
+            if (mouse.x == this.mouse.x || mouse.y == this.mouse.y) {
+                this.callback(null, event.ctrlKey);
+            }
+        }
+
+        this.controls.enabled = true
+    }
+
+    getMouseCoords(event) {
+        var mouseX = (event.offsetX / this.window.scrollWidth) * 2 - 1;
+        var mouseY = - (event.offsetY / this.window.scrollHeight) * 2 + 1;
+        return {x: mouseX, y: mouseY};
+    }
 
     raycast() {
         this.raycaster.setFromCamera(this.mouse, this.camera);
@@ -71,4 +83,17 @@ export class MouseListener {
         this.callback = callback;
     }
 
+    getIntersectPoint(intersects) {
+        var i = 0;
+        for (; i < intersects.length; i++) {
+            if (intersects[i].object.parent.visible == false) {
+                continue;
+            }
+            break;
+        }
+        var point = intersects[i].uv;
+        point.x = Math.floor(point.x * 64);
+        point.y = 63 - Math.floor(point.y * 64);
+        return point;
+    }
 }
