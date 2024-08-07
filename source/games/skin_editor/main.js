157f7bb2a074fc0634f5f2cf58a08cd98fc4fa13
JustAVirus <bern64390@gmail.com>
2024-08-07 16:42:00 +0200

    create color selection panel

--- source/games/skin_editor/main.js
+++ source/games/skin_editor/main.js
@@ -63,3 +63,51 @@ $('.part-select').click(function(event) {
 	var part = $(this).data('part');
 	player.togglePart(part);
 });
+
+
+
+class ColorSelector {
+	constructor() {
+		let temp = this;
+		$('.color-input').change(function() {
+			var type = $(this).attr('id');
+			temp.setColor(type, $(this).val());
+		});
+		this.color = [0, 0, 0];
+		this.opacity = 255;
+		this.updateColor();
+	}
+	
+	setColor(type, value) {
+		switch (type) {
+			case 'color':
+				value = value.substring(1);
+			case 'hex':
+				this.color = [parseInt(value.substring(0, 2), 16), parseInt(value.substring(2, 4), 16), parseInt(value.substring(4, 6), 16)];
+				break;
+			case 'A':
+				this.opacity = parseInt(value);
+				break;
+			case 'R':
+			case 'G':
+			case 'B':
+				this.color['RGB'.indexOf(type)] = parseInt(value);
+				break;
+		}
+		this.updateColor();
+	}
+
+	updateColor() {
+		$('#hex').val(this.colorToHex());
+		$('#R').val(this.color[0]);
+		$('#G').val(this.color[1]);
+		$('#B').val(this.color[2]);
+		$('#A').val(this.opacity);
+		$('#color').val("#" + this.colorToHex());
+	}
+	
+	colorToHex() {
+		return this.color[0].toString(16).padStart(2, '0') + this.color[1].toString(16).padStart(2, '0') + this.color[2].toString(16).padStart(2, '0');
+	}
+}
+const colorSelector = new ColorSelector();
--- source/games/skin_editor/tools/Pen.js
+++ source/games/skin_editor/tools/Pen.js
@@ -3,12 +3,12 @@ import { Tool } from './Tool.js';
 export class Pen extends Tool {
     constructor(layerList) {
         super(layerList);
-        this.addOption('color', '#37c4f2');
     }
 
     click(point, ctrlDown) {
         var g2d = this.layerList.currentLayer.canvas.getContext('2d');
-        g2d.fillStyle = this.getOption('color');
+        g2d.globalAlpha = $('#A').val() / 255;
+        g2d.fillStyle = $('#color').val();
         g2d.fillRect(point.x, point.y, 1, 1);
         this.layerList.onLayerUdated();
     }
