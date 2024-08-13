8a98d2b45caa37af300f3d6bde8b7b3ce602e8b8
JustAVirus <bern64390@gmail.com>
2024-08-13 23:38:39 +0200

    Add FloodFiller class for performing flood fill operations

---
+++ source/games/skin_editor/FloodFiller.js
@@ -0,0 +1,67 @@
+import { SkinMapper } from "./SkinMapper.js";
+
+export class FloodFiller {
+
+    constructor(data, tolerance, floodMode) {
+        this.data = data;
+        this.tolerance = tolerance;
+        this.floodMode = floodMode;
+        this.skinMapper = new SkinMapper();
+        this.selectedColor = [0, 0, 0, 0];
+        this.selection = new Array(64 * 64).fill(false);
+    }
+
+    startFloodFill(point) {
+        this.selectedColor = this.getColor(point.x, point.y);
+        if (this.floodMode === "global") {
+            for (let i = 0; i < 64; i++) {
+                for (let j = 0; j < 64; j++) {
+                    if (!this.skinMapper.onSkin({ x: i, y: j })) {
+                        continue;
+                    }
+                    if (this.colorDistance(this.getColor(i, j), this.selectedColor) > this.tolerance) {
+                        continue;
+                    }
+                    this.selection[i + j * 64] = true;
+                }
+            }
+        } else {
+            this.floodFill(point.x, point.y);
+        }
+        return this.selection;
+    }
+
+    floodFill(x, y) {
+        if (this.selection[x + y * 64]) {
+            return;
+        }
+        var color = this.getColor(x, y);
+        if (this.colorDistance(color, this.selectedColor) > this.tolerance) {
+            return;
+        }
+        this.layerList.selection[x + y * 64] = true;
+        var point = this.skinMapper.moveX({ x: x, y: y }, false);
+        this.floodFill(point.x, point.y);
+        point = this.skinMapper.moveX({ x: x, y: y }, true);
+        this.floodFill(point.x, point.y);
+        point = this.skinMapper.moveY({ x: x, y: y }, false);
+        this.floodFill(point.x, point.y);
+        point = this.skinMapper.moveY({ x: x, y: y }, true);
+        this.floodFill(point.x, point.y);
+        point = this.skinMapper.pointOnOppositeLayer({ x: x, y: y });
+        this.floodFill(point.x, point.y);
+    }
+
+    colorDistance(color1, color2) {
+        var diff = Math.sqrt(Math.pow(color1[0] - color2[0], 2) + Math.pow(color1[1] - color2[1], 2) + Math.pow(color1[2] - color2[2], 2));
+        var opacityDiff = 1 - Math.abs(color1[3] - color2[3]) / 255;
+        diff /= 441.6729559300637;
+        diff /= opacityDiff;
+        return diff;
+    }
+
+    getColor(x, y) {
+        var index = (x + y * 64) * 4;
+        return [this.data[index], this.data[index + 1], this.data[index + 2], this.data[index + 3]];
+    }
+}
--- source/games/skin_editor/tools/MagicWand.js
+++ source/games/skin_editor/tools/MagicWand.js
@@ -1,11 +1,10 @@
-import { SkinMapper } from "../SkinMapper.js";
+import { FloodFiller } from "../FloodFiller.js";
 import { SelectTool } from "./SelectTool.js";
 
 export class MagicWand extends SelectTool{
 
     constructor(layerList) {
         super(layerList);
-        this.skinMapper = new SkinMapper();
         this.selectedColor = [0, 0, 0, 0];
         this.addOption('tolerance', 0.10);
         this.addOption('floodMode', "global");
@@ -16,63 +15,18 @@ export class MagicWand extends SelectTool{
             this.clearSelection();
             return;
         }
-        if (!ctrlDown) {
-            this.clearSelection();
-        }
         var g2d = this.layerList.currentLayer.canvas.getContext('2d');
         this.data = g2d.getImageData(0, 0, 64, 64).data;
-        this.selectedColor = this.getColor(point.x, point.y);
-        if (this.getOption('floodMode') === "global") {
-            for (let i = 0; i < 64; i++) {
-                for (let j = 0; j < 64; j++) {
-                    if (!this.skinMapper.onSkin({ x: i, y: j })) {
-                        continue;
-                    }
-                    if (this.colorDistance(this.getColor(i, j), this.selectedColor) > this.getOption('tolerance')) {
-
-                        continue;
-                    }
-                    this.layerList.selection[i + j * 64] = true;
-                }
+        let floodFiller = new FloodFiller(this.data, this.getOption('tolerance'), this.getOption('floodMode'));
+        this.selection = floodFiller.startFloodFill(point);
+        if (ctrlDown) {
+            for (let i = 0; i < this.layerList.selection.length; i++) {
+                this.layerList.selection[i] = this.layerList.selection[i] || this.selection[i];
             }
         } else {
-            this.floodFill(point.x, point.y);
+            this.clearSelection();
+            this.layerList.selection = this.selection;
         }
         this.updateSelectionCanvas();
-    }
-
-    floodFill(x, y) {
-        if (this.layerList.selection[x + y * 64]) {
-            return;
-        }
-        var color = this.getColor(x, y);
-        if (this.colorDistance(color, this.selectedColor) > this.getOption('tolerance')) {
-            return;
-        }
-        this.layerList.selection[x + y * 64] = true;
-        var point = this.skinMapper.moveX({ x: x, y: y }, false);
-        this.floodFill(point.x, point.y);
-        point = this.skinMapper.moveX({ x: x, y: y }, true);
-        this.floodFill(point.x, point.y);
-        point = this.skinMapper.moveY({ x: x, y: y }, false);
-        this.floodFill(point.x, point.y);
-        point = this.skinMapper.moveY({ x: x, y: y }, true);
-        this.floodFill(point.x, point.y);
-        point = this.skinMapper.pointOnOppositeLayer({ x: x, y: y });
-        this.floodFill(point.x, point.y);
-    }
-
-    getBaseColor() {
-        return this.selectedColor;
-    }
-
-    colorDistance(color1, color2) {
-        var diff = Math.sqrt(Math.pow(color1[0] - color2[0], 2) + Math.pow(color1[1] - color2[1], 2) + Math.pow(color1[2] - color2[2], 2));
-        return diff / 441.6729559300637;
-    }
-
-    getColor(x, y) {
-        var index = (y * 64 + x) * 4;
-        return [this.data[index], this.data[index + 1], this.data[index + 2], this.data[index + 3]];
     }
 }
