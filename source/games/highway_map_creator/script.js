e93b62f358940fbf2fe7f40b89a819be03cff912
JustAVirus <91348480+NotJustAVirus@users.noreply.github.com>
2025-07-29 22:54:47 +0200

    Add initial files of highway map creator with banners and styles

---
+++ source/games/highway_map_creator/script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+    const canvas = document.getElementById('mapCanvas');
+    const ctx = canvas.getContext('2d');
+    const poiList = document.getElementById('objectList').querySelector('.poi-list');
+
+    const banners = [
+        "black_banner",
+        "blue_banner",
+        "brown_banner",
+        "cyan_banner",
+        "gray_banner",
+        "green_banner",
+        "light_blue_banner",
+        "light_gray_banner",
+        "lime_banner",
+        "magenta_banner",
+        "orange_banner",
+        "pink_banner",
+        "purple_banner",
+        "red_banner",
+        "white_banner",
+        "yellow_banner"
+    ];
+
+});
+
