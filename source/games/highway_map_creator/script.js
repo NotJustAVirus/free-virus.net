d6b62d14d37d35c21e5a10f27f0c0a38b41a1540
JustAVirus <91348480+NotJustAVirus@users.noreply.github.com>
2025-07-30 23:26:59 +0200

    Update canvas setup

--- source/games/highway_map_creator/script.js
+++ source/games/highway_map_creator/script.js
@@ -1,7 +1,9 @@
 
 $(document).ready(function(){
-    const canvas = document.getElementById('mapCanvas');
-    const ctx = canvas.getContext('2d');
+    const backgroundCanvas = document.getElementById('backgroundCanvas');
+    const backgroundCtx = backgroundCanvas.getContext('2d');
+    const mainCanvas = document.getElementById('mainCanvas');
+    const mainCtx = mainCanvas.getContext('2d');
     const poiList = document.getElementById('objectList').querySelector('.poi-list');
 
     const banners = [
@@ -23,5 +25,26 @@ $(document).ready(function(){
         "yellow_banner"
     ];
 
+    // draw checkered background on main canvas
+    function drawCheckeredBackground() {
+        const size = 1; // size of each square
+        for (let x = 0; x < mainCanvas.width; x += size) {
+            for (let y = 0; y < mainCanvas.height; y += size) {
+                mainCtx.fillStyle = (x / size + y / size) % 2 === 0 ? '#f0f0f0' : '#303030ff';
+                mainCtx.fillRect(x, y, size, size);
+            }
+        }
+    }
+    drawCheckeredBackground();
+
+    function drawMap() {
+        const img = new Image();
+        img.src = 'images/map_background.png';
+        img.onload = function() {
+            backgroundCtx.drawImage(img, 0, 0, backgroundCanvas.width, backgroundCanvas.height);
+        };
+    }
+
+    drawMap();
 });
 
